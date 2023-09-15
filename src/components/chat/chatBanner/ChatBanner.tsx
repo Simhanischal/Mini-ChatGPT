c4184336518cc40c5e2c1869abff852d53611f2d
Nischal Simha <nischalsimhas@gmail.com>
2023-09-15 21:09:08 +0530

    using the existing image for profile picture instead of hardcoding image url

--- src/components/chat/chatBanner/ChatBanner.tsx
+++ src/components/chat/chatBanner/ChatBanner.tsx
@@ -1,6 +1,6 @@
-// import Image from "../../../Images/logo192.png";
 import Typography from "@material-ui/core/Typography";
 import { green } from "@material-ui/core/colors";
+import Image from "../../../assets/images/logo192.png";
 import '../../../assets/stylesheets/chat/chatBanner.scss'
 
 const ChatBanner = () => {
@@ -8,7 +8,7 @@ const ChatBanner = () => {
     <>
       <div className="banner">
         <div className="bannerAvatar">
-          <img alt="Chat Avatar" src='https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-1024.png' width="20" />
+          <img alt="Chat Avatar" src={Image} width="20" />
         </div>
         <div className="bannerBotDetails">
           <Typography variant="body2">Mini ChatGPT</Typography>
