ed8887a8abd3d59a8a551d2101cd47babb4a748e
Nischal Simha <33859648+Simhanischal@users.noreply.github.com>
2023-09-15 19:25:13 +0530

    Implement Welcome Circle component (#1)
    * Implement Welcome Circle component
    
    * Changed folder structure and fixed imports

---
+++ src/components/home/welcomeCircle/WelcomeCircle.tsx
@@ -0,0 +1,45 @@
+import '../../../assets/stylesheets/home/welcomeCircle.scss';
+import Badge from '@material-ui/core/Badge';
+import QuestionAnswerIconOutlined from '@material-ui/icons/QuestionAnswerOutlined';
+import CloseIcon from '@material-ui/icons/Close';
+
+interface WelcomeCircleProps {
+  changeIcon: boolean;
+  welcomeMessage: string;
+  handleChangeIcon: () => void;
+  image: string;
+  width: number;
+}
+
+const WelcomeCircle = ({changeIcon, welcomeMessage, handleChangeIcon, image, width}: WelcomeCircleProps) => {
+  let display, icon;
+
+  if (changeIcon) {
+    icon = <CloseIcon style={{ fontSize: 40, color: "#5851DB" }} />
+  } else{
+    icon = <QuestionAnswerIconOutlined style={{ fontSize: 40, color: "#5851DB" }} />
+  }
+
+  if (welcomeMessage) {
+    display = (
+      <>
+        &nbsp;&nbsp;
+        <Badge badgeContent={1} overlap="circular" color="error">
+          <button className="avatar" onClick={handleChangeIcon}>
+            <img alt="avatar" src={image} width={width} />
+          </button>
+        </Badge> 
+      </>
+    );
+  } else{
+    display = (
+      <button className="avatar" onClick={handleChangeIcon}>
+        { icon }
+      </button>
+    );
+  }
+
+  return display;
+}
+
+export default WelcomeCircle;
