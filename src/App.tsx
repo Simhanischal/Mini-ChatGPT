2a731727ea6ee0b6ebd76e742c93a85c64c72ffb
Nischal Simha <33859648+Simhanischal@users.noreply.github.com>
2023-09-15 21:12:13 +0530

    Implement ChatBanner component (#3)
    * chatbanner
    
    * using the existing image for profile picture instead of hardcoding image url

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      Initial Setup!
+     Initial Setup!
     </>
   )
 }
---
+++ src/components/chat/chatBanner/ChatBanner.tsx
@@ -0,0 +1,27 @@
+import Typography from "@material-ui/core/Typography";
+import { green } from "@material-ui/core/colors";
+import Image from "../../../assets/images/logo192.png";
+import '../../../assets/stylesheets/chat/chatBanner.scss'
+
+const ChatBanner = () => {
+  return (
+    <>
+      <div className="banner">
+        <div className="bannerAvatar">
+          <img alt="Chat Avatar" src={Image} width="20" />
+        </div>
+        <div className="bannerBotDetails">
+          <Typography variant="body2">Mini ChatGPT</Typography>
+          <Typography
+            variant="caption"
+            style={{ fontSize: 11, color: green[300] }}
+          >
+            Online
+          </Typography>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ChatBanner;
