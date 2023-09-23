558413ea542c04f6224e14c37565701b39824b88
Nischal Simha <nischalsimhas@gmail.com>
2023-09-24 00:18:48 +0530

    Modify tests to incorporate error handling logic

--- tests/ChatInput.test.tsx
+++ tests/ChatInput.test.tsx
@@ -7,26 +7,31 @@ const handleInputChange = jest.fn();
 const handleEnter = jest.fn();
 const handleSend = jest.fn();
 
-test('displays the send icon when input prop string length is gretaer than 0', async () => {
-  render(<ChatInput input={input} handleInputChange={handleInputChange} handleEnter={handleEnter} handleSend={handleSend} />);
+test('displays the send icon when input prop string length is gretaer than 0 and status is success', async () => {
+  render(<ChatInput input={input} handleInputChange={handleInputChange} handleEnter={handleEnter} handleSend={handleSend} status='success' />);
   await screen.findByTestId('TelegramIcon');
   expect(screen.getByTestId('TelegramIcon')).toBeVisible();
 });
 
 test('does not display the send icon when input prop string length is 0', async () => {
-  render(<ChatInput input="" handleInputChange={handleInputChange} handleEnter={handleEnter} handleSend={handleSend} />);
+  render(<ChatInput input="" handleInputChange={handleInputChange} handleEnter={handleEnter} handleSend={handleSend} status='success' />);
+  expect(screen.queryByTestId('TelegramIcon')).toBeNull();
+});
+
+test('does not display the send icon when status is failed or retrying', async () => {
+  render(<ChatInput input="test" handleInputChange={handleInputChange} handleEnter={handleEnter} handleSend={handleSend} status='failed' />);
   expect(screen.queryByTestId('TelegramIcon')).toBeNull();
 });
 
 test('calls the event handler upon clicking on send button', async () => {
-  render(<ChatInput input={input} handleInputChange={handleInputChange} handleEnter={handleEnter} handleSend={handleSend} />);
+  render(<ChatInput input={input} handleInputChange={handleInputChange} handleEnter={handleEnter} handleSend={handleSend} status='success' />);
   await screen.findByTestId('TelegramIcon');
   fireEvent.click(screen.getByTestId('TelegramIcon'));
   expect(handleSend).toHaveBeenCalledTimes(1);
 });
 
 test('calls the event handler upon clicking on enter in keyboard', async () => {
-  render(<ChatInput input={input} handleInputChange={handleInputChange} handleEnter={handleEnter} handleSend={handleSend} />);
+  render(<ChatInput input={input} handleInputChange={handleInputChange} handleEnter={handleEnter} handleSend={handleSend} status='' />);
   await screen.findByTestId('TelegramIcon');
   fireEvent.keyDown(screen.getByTestId('chat-text-area'), {key: 'Enter'});
   expect(handleEnter).toHaveBeenCalledTimes(1);
